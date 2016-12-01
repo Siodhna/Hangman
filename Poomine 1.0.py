0252d18025fa50fd19e0a90894b857b1984be6bb
Asuna15 <kreete@ekstreem.ee>
2016-12-01 23:23:04 +0200

    algus

--- Poomine 1.0.py
+++ Poomine 1.0.py
@@ -1,3 +1,24 @@
 import pygame
 
+pygame.init()
+
+Screen = pygame.display.set_mode((win_width, win_height))  # teeb akna
+pygame.display.set_caption("Hangman")  # aknale pealkiri
+
+
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+        if event.type == pygame.MOUSEBUTTONUP:
+
+
+
+    pygame.display.update()
+
+    pygame.quit()
+    quit()
 
